b9cff4715d2fc9f1faebfb20930507f7a347123e
olawalecodes <iwaloyeo@gmail.com>
2021-12-03 23:08:53 +0100

    updates

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import logo from "../../assets/images/logo3.svg";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import * as GiIcons from "react-icons/gi";
 import * as AiIcons from 'react-icons/ai';
 
@@ -10,13 +10,23 @@ const Navbar = () => {
     e.preventDefault();
     setopennav(!opennav);
   }
+
+  //Assigning location to a variable
+  const location = useLocation();
+
+  //Destrucuring pathname from location
+  const { pathname } = location;
+
+  const splitLocation = pathname.split("/")
+
+  //Javascript split method to get the name of the path in array
   return (
     <header className="navbar">
       <img src={logo} alt="Logo" className="logo" />
       <button className="nav-toggle" onClick={(e) => handleNavToggle(e)}>{opennav ? <AiIcons.AiOutlineClose /> : <GiIcons.GiHamburgerMenu />}</button>
       <nav>
         <ul className={opennav ? "navlinks sr-only" : "navlinks"} >
-          <li><Link to="/">Home</Link></li>
+          <li className={splitLocation[1] === "" ? "active" : ""}><Link to="/">Home</Link></li>
           <li><Link to="/">About</Link></li>
           <li><Link to="/">Work</Link></li>
           <li><Link to="/">Resume</Link></li>
