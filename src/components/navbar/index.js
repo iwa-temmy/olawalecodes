0e35109ae697201032c5589ce3a83ae3535496f5
olawalecodes <iwaloyeo@gmail.com>
2021-12-03 11:27:22 +0100

    updates on mobile nav

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from "../../assets/images/logo3.svg";
 import { Link } from 'react-router-dom';
+import * as GiIcons from "react-icons/gi";
 
 const Navbar = () => {
+  const [opennav, setopennav] = useState(false);
+  const handleNavToggle = (e) => {
+    e.preventDefault();
+    setopennav(!opennav);
+  }
   return (
     <header className="navbar">
       <img src={logo} alt="Logo" className="logo" />
-
+      <button className="nav-toggle" onClick={(e) => handleNavToggle(e)}><GiIcons.GiHamburgerMenu /></button>
       <nav>
-        <ul className="navlinks">
-          <li><Link>Home</Link></li>
-          <li><Link>About</Link></li>
-          <li><Link>Work</Link></li>
-          <li><Link>Resume</Link></li>
+        <ul className={opennav ? "navlinks" : ""}>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/">About</Link></li>
+          <li><Link to="/">Work</Link></li>
+          <li><Link to="/">Resume</Link></li>
         </ul>
       </nav>
     </header>
