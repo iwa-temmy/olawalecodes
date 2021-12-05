57f17b39332d6e071821cb1e50eb2a5f170e41b0
olawalecodes <iwaloyeo@gmail.com>
2021-12-05 14:08:58 +0100

    my works stage in progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/home";
 import About from './components/about';
+import Work from "./components/works";
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/about" component={About} exact />
+        <Route path="/work" component={Work} exact />
       </Switch>
     </div>
   );
---
+++ src/components/works/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Navbar from '../navbar';
+
+const Works = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>My Works</h1>
+      <p>Here are some of the projects I have worked on </p>
+      <section>
+        <div className="cards">
+          <div className="card">
+            <h2>Project Name</h2>
+            <p>Project Description</p>
+            <div className="stack">
+              <p>Html</p>
+              <p>Css</p>
+              <p>Javascript</p>
+            </div>
+          </div>
+          <div className="card">
+            <h2>Project Name</h2>
+            <p>Project Description</p>
+            <div className="stack">
+              <p>Html</p>
+              <p>Css</p>
+              <p>Javascript</p>
+            </div>
+          </div>
+          <div className="card">
+            <h2>Project Name</h2>
+            <p>Project Description</p>
+            <div className="stack">
+              <p>Html</p>
+              <p>Css</p>
+              <p>Javascript</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Works;
