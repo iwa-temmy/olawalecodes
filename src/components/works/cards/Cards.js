7fe35a43958d408c2e42f13a85f6e55074f699e3
olawalecodes <iwaloyeo@gmail.com>
2021-12-06 06:07:37 +0100

    permanent fix for z-index bug

---
+++ src/components/works/cards/Cards.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '../card/Card';
+
+const cards = () => {
+  return (
+    <div className="cards">
+      <Card />
+    </div>
+  )
+}
+
+export default cards
---
+++ src/database.js
@@ -0,0 +1,9 @@
+export const projects = [
+  {
+    id: 1,
+    title: "",
+    description: "",
+    github_url: "",
+    app_url: "",
+  }
+]
