a15edc8ea51abe49944d51ff5cead8a2c2b9c25a
olawalecodes <iwaloyeo@gmail.com>
2022-05-17 14:46:34 +0100

    updated

--- src/database.js
+++ src/database.js
@@ -5,7 +5,7 @@ export const projects = [
     description: "A simple todo list that helps to manage your daily tasks and retains data after reloading the page.",
     github_url: "https://github.com/iwa-temmy/todo-app",
     app_url: "https://devctodoapp.netlify.app",
-    stack: ["React", "Sass", "LocalStorage"]
+    stack: ["React JS", "Sass", "LocalStorage"]
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const projects = [
     description: "The official website of the Fuse ERP software that gives potential clients insights about the features of the application",
     github_url: "",
     app_url: "http://fuseerp.com/",
-    stack: ["React", "SASS"]
+    stack: ["React JS", "SASS"]
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const projects = [
     description: "An encylopedia of the cast of the Breaking Bad TV Series",
     github_url: "https://github.com/iwa-temmy/breaking-bad-app",
     app_url: "https://breakingbadencyclopedia.netlify.app/",
-    stack: ["React", "CSS", "Breaking Bad API"]
+    stack: ["React JS", "CSS", "Breaking Bad API"]
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ export const projects = [
     description: "A Mini Library to manage and track books that have been read, currently reading, and want to read",
     github_url: "https://github.com/iwa-temmy/todo-app",
     app_url: "https://olawalebookstore.netlify.app",
-    stack: ["React", "CSS"]
+    stack: ["React JS", "CSS"]
   },
   {
     id: 5,
@@ -37,6 +37,6 @@ export const projects = [
     description: "SmartComply is a solution that helps payment card industry organizations in attaining their compliance in a short duration of time.",
     github_url: "",
     app_url: "https://app.smartcomplyapp.com/auth/login",
-    stack: ["React", "CSS", "Material UI", "React - Redux", "SASS"]
+    stack: ["React JS", "CSS", "Material UI", "React - Redux", "SASS"]
   },
 ]
